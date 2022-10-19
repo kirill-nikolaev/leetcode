e6aff7838de5d57df84c8ad64214e3c6b5aa5760
Kirill Nikolaev <kirill.nikolaev.an@gmail.com>
2022-10-19 14:42:42 +0300

    add 692

--- src/main/java/solutions/leetcode/Test.java
+++ src/main/java/solutions/leetcode/Test.java
@@ -4,9 +4,5 @@ import java.time.LocalDateTime;
 
 public class Test {
     public static void main(String[] args) {
-
-        LocalDateTime now = LocalDateTime.now();
-        System.out.println(now);
-        System.out.println(now.plusDays(10));
     }
 }
---
+++ src/main/java/solutions/leetcode/solution692/Solution.java
@@ -0,0 +1,56 @@
+package solutions.leetcode.solution692;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String, Integer> wordFrequencyMap = new HashMap<>();
+        for (int i = 0; i < words.length; i++)
+            wordFrequencyMap.put(words[i], wordFrequencyMap.getOrDefault(words[i], 0) + 1);
+
+
+        List<String> result = wordFrequencyMap.entrySet().stream().
+                map(entry->new WordFrequency(entry.getKey(), entry.getValue())).
+                sorted().
+                limit(k).
+                map(WordFrequency::getWord)
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    private class WordFrequency implements Comparable<WordFrequency>{
+        private final String word;
+        private final int frequency;
+
+        public WordFrequency(String word, int frequency) {
+            this.word = word;
+            this.frequency = frequency;
+        }
+
+        public String getWord() {
+            return word;
+        }
+
+        public int getFrequency() {
+            return frequency;
+        }
+
+        @Override
+        public int compareTo(WordFrequency wordFrequency) {
+            if (this.frequency != wordFrequency.getFrequency())
+                return wordFrequency.getFrequency() - this.frequency;
+            else
+                return word.compareTo(wordFrequency.getWord());
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.topKFrequent(new String[]{"i","love","leetcode","i","love","coding"}, 2));
+        System.out.println(solution.topKFrequent(new String[]{"the","day","is","sunny","the","the","the","sunny","is","is"}, 4));
+    }
+}
